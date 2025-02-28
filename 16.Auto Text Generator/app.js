65bc404f3eef03987aae423cb7180d15a0407515
jintorajp <jintorajp@gmail.com>
2025-02-28 16:28:34 +0530

    Auto Text Generator

---
+++ 16.Auto Text Generator/app.js
@@ -0,0 +1,37 @@
+const typedText = document.querySelector(".typed-text");
+const words= ['AWESOME',"FUN","LOVING","EASY","RICH","POWERFULL"];
+let index = 0;
+let charIndex = 0;
+const typeDelay=200;
+const eraseDelay=200;
+const newLetterDelay=2000;
+document.addEventListener("DOMContentLoaded",()=>{
+    if(words.length){
+        setTimeout(type,newLetterDelay);
+    }
+});
+function type(){
+    if(charIndex < words[index].length){
+        typedText.textContent += words[index].charAt(charIndex); 
+        charIndex++;
+        setTimeout(type,typeDelay)
+    }
+    else{
+        setTimeout(erase,newLetterDelay);
+    }
+}
+function erase(){
+    if(charIndex > 0){
+        typedText.textContent = words[index].substring(0,charIndex -1);
+        charIndex--
+        setTimeout(erase,eraseDelay);
+    }
+    else{
+        index++;
+        if(index >= words.length){
+            index =0;
+        }
+        setTimeout(type, typeDelay + 1100);
+
+    }
+}
